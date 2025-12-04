f11807b8512ec544f2f9a7f8c96dc0d41f0ca00b
akazdayo <82073147+akazdayo@users.noreply.github.com>
2025-12-05 01:29:36 +0900

    Reapply "add sstの実装のモジュール化"
    This reverts commit dfccf07597d2affc18ee31b9a3bca65c8260edcc.

---
+++ apps/web/src/lib/sst/index.ts
@@ -0,0 +1,21 @@
+export { SpeechRecognitionClient } from "./speech";
+export type {
+  UseSpeechRecognitionOptions,
+  UseSpeechRecognitionReturn,
+} from "./hooks";
+export { useSpeechRecognition } from "./hooks";
+export type {
+  SpeechRecognition,
+  SpeechRecognitionAlternative,
+  SpeechRecognitionCallbacks,
+  SpeechRecognitionConfig,
+  SpeechRecognitionConstructor,
+  SpeechRecognitionError,
+  SpeechRecognitionErrorCode,
+  SpeechRecognitionErrorEvent,
+  SpeechRecognitionEvent,
+  SpeechRecognitionResult,
+  SpeechRecognitionResultList,
+  SpeechRecognitionState,
+  SupportedLanguage,
+} from "./types/sst";
