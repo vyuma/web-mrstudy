5f7c985ada5b1acdff5c9bd4c0a31fc1a1f7128d
akazdayo <82073147+akazdayo@users.noreply.github.com>
2025-12-05 20:53:24 +0900

    feat: Agent aliceを実装

--- apps/backend/src/mastra/index.ts
+++ apps/backend/src/mastra/index.ts
@@ -1,29 +1,36 @@
-
-import { Mastra } from '@mastra/core/mastra';
-import { PinoLogger } from '@mastra/loggers';
-import { LibSQLStore } from '@mastra/libsql';
-import { weatherWorkflow } from './workflows/weather-workflow';
-import { weatherAgent } from './agents/weather-agent';
-import { toolCallAppropriatenessScorer, completenessScorer, translationScorer } from './scorers/weather-scorer';
+import { Mastra } from "@mastra/core/mastra";
+import { LibSQLStore } from "@mastra/libsql";
+import { PinoLogger } from "@mastra/loggers";
+import { weatherAgent } from "./agents/weather-agent";
+import {
+	completenessScorer,
+	toolCallAppropriatenessScorer,
+	translationScorer,
+} from "./scorers/weather-scorer";
+import { weatherWorkflow } from "./workflows/weather-workflow";
 
 export const mastra = new Mastra({
-  workflows: { weatherWorkflow },
-  agents: { weatherAgent },
-  scorers: { toolCallAppropriatenessScorer, completenessScorer, translationScorer },
-  storage: new LibSQLStore({
-    // stores observability, scores, ... into memory storage, if it needs to persist, change to file:../mastra.db
-    url: ":memory:",
-  }),
-  logger: new PinoLogger({
-    name: 'Mastra',
-    level: 'info',
-  }),
-  telemetry: {
-    // Telemetry is deprecated and will be removed in the Nov 4th release
-    enabled: false, 
-  },
-  observability: {
-    // Enables DefaultExporter and CloudExporter for AI tracing
-    default: { enabled: true }, 
-  },
+	workflows: { weatherWorkflow },
+	agents: { weatherAgent },
+	scorers: {
+		toolCallAppropriatenessScorer,
+		completenessScorer,
+		translationScorer,
+	},
+	storage: new LibSQLStore({
+		// stores observability, scores, ... into memory storage, if it needs to persist, change to file:../mastra.db
+		url: ":memory:",
+	}),
+	logger: new PinoLogger({
+		name: "Mastra",
+		level: "info",
+	}),
+	telemetry: {
+		// Telemetry is deprecated and will be removed in the Nov 4th release
+		enabled: false,
+	},
+	observability: {
+		// Enables DefaultExporter and CloudExporter for AI tracing
+		default: { enabled: true },
+	},
 });
