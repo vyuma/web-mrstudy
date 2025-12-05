c34a3669791a59c87d9168d252bea546e9a4c25e
akazdayo <82073147+akazdayo@users.noreply.github.com>
2025-12-05 19:43:47 +0900

    chore: mastra単体にした

--- apps/backend/src/mastra/index.ts
+++ apps/backend/src/mastra/index.ts
@@ -1,15 +1,29 @@
-import { Mastra } from "@mastra/core/mastra";
-import { weatherAgent } from "./agents/weather-agent";
+
+import { Mastra } from '@mastra/core/mastra';
+import { PinoLogger } from '@mastra/loggers';
+import { LibSQLStore } from '@mastra/libsql';
+import { weatherWorkflow } from './workflows/weather-workflow';
+import { weatherAgent } from './agents/weather-agent';
+import { toolCallAppropriatenessScorer, completenessScorer, translationScorer } from './scorers/weather-scorer';
 
 export const mastra = new Mastra({
-  agents: {
-    weatherAgent,
-  },
+  workflows: { weatherWorkflow },
+  agents: { weatherAgent },
+  scorers: { toolCallAppropriatenessScorer, completenessScorer, translationScorer },
+  storage: new LibSQLStore({
+    // stores observability, scores, ... into memory storage, if it needs to persist, change to file:../mastra.db
+    url: ":memory:",
+  }),
+  logger: new PinoLogger({
+    name: 'Mastra',
+    level: 'info',
+  }),
   telemetry: {
-    enabled: false,
+    // Telemetry is deprecated and will be removed in the Nov 4th release
+    enabled: false, 
   },
-  server: {
-    port: 4111, // Defaults to 4111
-    timeout: 10000, // Defaults to 3 * 60 * 1000 (3 minutes)
+  observability: {
+    // Enables DefaultExporter and CloudExporter for AI tracing
+    default: { enabled: true }, 
   },
 });
